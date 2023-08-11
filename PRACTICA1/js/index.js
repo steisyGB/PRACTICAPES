4c3a1185e6dd7eeb52c629448e175dcb9184cde0
steisyGB <142046596+steisyGB@users.noreply.github.com>
2023-08-11 12:04:39 -0600

    COMMIT INICIAL

---
+++ PRACTICA1/js/index.js
@@ -0,0 +1,27 @@
+// let edad = prompt("Introduzca su edad")
+
+// if(edad > 18 && edad <= 100){
+//     alert("el usuario es mayor de edad")
+// }else if(edad < 18 && edad >= 1){
+//     alert("el usuario es menor de edad")
+// }else if(edad == 18){
+//     alert("el usuario tiene 18")
+// }else if(edad > 100){
+//     alert("el usuario es centenario")
+// }else if(edad <= 0){
+//     alert("edad invalido")
+
+// }
+
+// let i = 1;
+// let suma = 0;
+// while(i <=100){
+//     suma += i;
+//     i++;
+// }
+
+// alert(suma)
+let num = prompt("ingrese un numero")
+for(i = 1; i <= 10; i++){
+    alert(num + " por " + i + "es igual a: " + (num*i))
+}
