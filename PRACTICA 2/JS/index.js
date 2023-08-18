212e2b436c94fe111b5825f82eb3b4668074f99d
steisyGB <142046596+steisyGB@users.noreply.github.com>
2023-08-18 12:07:06 -0600

    Add files via upload

---
+++ PRACTICA 2/JS/index.js
@@ -0,0 +1,58 @@
+// let numeros = []
+// let otrosnumeros = []
+// let arrayFinal = []
+// for (let i = 1; i <= 100; i++){
+//     let mensaje = `el valor es: ${i}`
+//     numeros.push(mensaje)
+// }
+
+// for (let i = 101; i <= 200; i++){
+//     let mensaje = `el valor es: ${i}`
+//     numeros.push(mensaje)
+// }
+
+// arrayFinal = [...numeros, ...otrosnumeros ]
+// arrayFinal.forEach((i) => {
+//     console.log(i)
+// })
+
+// for(let i= 1; i<=100; i++){
+//     let mensaje = `el valor es: ${i}`
+//     numeros.push(i)
+// }
+
+// numeros.forEach( (i) =>{
+//     console.log(i)
+// } )
+
+
+
+let persona = {
+    nombre: `steisy`,
+    edad: `17`,
+    carrera: `Sofware` 
+}
+
+let persona2 = {
+    nombre: `steisy`,
+    edad: `17`,
+    carrera: `arquitectura` 
+}
+
+let persona3 = {
+    nombre: `steisy`,
+    edad: `17`,
+    carrera: `Sofware` 
+}
+
+let = arrayObjetos = [persona, persona2, persona3]
+
+arrayObjetos.forEach((i) => {
+    console.log(i)
+})
+
+console.log(arrayObjetos[1].carrera)
+
+//imprimir el nombre de la posicion 0
+//imprimir la carrera de la posicion 1
+//imprimir la edad de la posicion 2
